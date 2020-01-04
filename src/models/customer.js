ae2c5cc7725a165127966f3492efb8c178254a49
drogba <drogba@wink.kr>
2020-01-04 18:04:14 +0900

    router edit

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/mongodb_tutorial');
+
+autoIncrement.initialize(connection);
 
 const customerSchema = new mongoose.Schema({
     id: {type: Number},
@@ -9,5 +13,13 @@ const customerSchema = new mongoose.Schema({
     job: {type: String}
 });
 
+customerSchema.plugin(autoIncrement.plugin , {
+    model: 'customer',
+    field: 'id',
+    startAt: 4,
+    increment: 1
+});
 
+
+const Customer = connection.model('customer' , customerSchema);
 module.exports = mongoose.model('customer' , customerSchema);
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const memberRouter = require('./member/member');
+const customerRouter = require('./customer/customer');
 
 console.log('Router...')
 router.use('/member' , memberRouter);
+router.use('/customer' , customerRouter);
 
-module.exports = router;
+module.exports = router
