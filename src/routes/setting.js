6920563655d57c833edbcc5d44126e373846ea49
drogba <drogba02@naver.com>
2020-01-13 20:46:04 +0900

    middleware add

--- src/routes/setting.js
+++ src/routes/setting.js
@@ -6,16 +6,21 @@ const fs = require('fs');
 const session = require('express-session');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(session({
     secret: 'drogbaSession',
     resave: false,
     saveUninitialized: true
 }));
 app.use(morgan);
+app.use(cors());
 
 console.log('Server js !!!');
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const db = require('./routes/dbConnection');
 const router = require('./routes/router');
 const setting = require('./routes/setting');
+const expressErrorHandler = require('express-error-handler');
+const errorHandler = expressErrorHandler({
+    static: {
+        '404':'./public/404.html'
+    }
+});
 
 app.use(db);
 app.use(setting);
@@ -12,6 +18,8 @@ app.use('/dr' , router);
 console.log('dirname:'+__dirname);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
 app.listen(port , () => {
