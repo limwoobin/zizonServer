46a34ff7a8f99244d028dad3642dc78eee68ca94
limwoobin <drogba@wink.kr>
2020-01-24 19:09:58 +0900

    findbyemail api add

--- src/common/config.js
+++ src/common/config.js
@@ -7,30 +7,3 @@
 
 
 // PBKDF2 - secure password
-var pbkdf2Password = require('pbkdf2-password');
-var hasher = pbkdf2Password();
-
-router.get('/regist', function (req, res) {
-    res.render('regist', { title: '36.5 Arts' });
-});
-
-router.post('/regist', function (req, res) {
-
-    hasher({password: req.body.password}, function (err, pass, salt, hash) {
-        var user = new User();
-        user.email = req.body.email;
-        user.password = hash;
-        user.salt = salt;
-        user.type = req.body.type;
-        user.major = req.body.major;
-
-        user.save(function (err) {
-            if(err){
-                console.log(err);
-                res.redirect('/');
-            }
-            req.session.email = user.email;
-            res.redirect('/');
-        })
-    });
-});
--- src/routes/member/member.js
+++ src/routes/member/member.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Member = require('../../models/member');
 const common = require('../../common/common');
+const crypto = require('crypto');
+const pbkdf2Password = require('pbkdf2-password');
 
 router.get('/members' , (req , res) => {
     console.log('findAll...');
@@ -11,29 +13,82 @@ router.get('/members' , (req , res) => {
     })
 });
 
-router.post('/insert' , (req , res) => {
-    console.log(req.body);
-    let member = new Member();
-    member.userEmail = req.body.userEmail;
-    member.userPwd = req.body.userPwd;
-    member.birthday = req.body.birthday;
-    member.userNm = req.body.userNm;
-    member.userPhone = req.body.userPhone;
-    
-    // id 찾기 , 패스워드 찾기 로직
-    
-    member.save((err) => {
-        if(err){
-            console.error(err);
-            common.result.code = 'DRG01';
-            common.result.message = common.status.DRG01;
+router.get('/overlap/check/:userEmail' , (req , res) => {
+    console.log('param:' + req.params.userEmail);
+    common.res = {};
+    Member.findOne({userEmail:req.params.userEmail} , (err , member) => {
+        console.log('mem:' + member);
+        if(err) {
+            console.log('err:' + err);
+            throw err;
+        }
+        if(!member) {
+            common.result.code = 'DRG00';
+            common.result.message = common.status.DRG00;
             res.json(common.result);
             return;
+        }else{
+            common.result.code = 'DRG01';
+            common.result.message = common.status.DRG01;
+            common.result.res = member;
+            res.json(common.result);
         }
-        common.result.code = 'DRG00';
-        common.result.message = common.status.DRG00;
-        res.json(common.result);
+    })
+});
+
+
+router.post('/insert' , (req , res) => {
+    console.log(req.body);
+    // let member = new Member();
+    // member.userEmail = req.body.userEmail;
+    // member.userPwd = req.body.userPwd;
+    // member.birthday = req.body.birthday;
+    // member.userNm = req.body.userNm;
+    // member.userPhone = req.body.userPhone;
+    
+    // password encrypt
+    crypto.randomBytes(64, (err, buf) => {
+        crypto.pbkdf2(req.body.userPwd , buf.toString('base64'), 102391, 64, 'sha512', (err, key) => {
+            let member = new Member();
+            member.userEmail = req.body.userEmail;
+            member.userPwd = key.toString('base64');
+            member.salt = buf.toString('base64');
+            member.birthday = req.body.birthday;
+            member.userNm = req.body.userNm;
+            member.userPhone = req.body.userPhone;
+
+            member.save((err) => {
+                if(err){
+                    console.error(err);
+                    common.result.code = 'DRG01';
+                    common.result.message = common.status.DRG01;
+                    res.json(common.result);
+                    return;
+                }
+                common.result.code = 'DRG00';
+                common.result.message = common.status.DRG00;
+                res.json(common.result);
+            });
+        });
     });
+
+    
+
+
+    // id 찾기 , 패스워드 찾기 로직
+    
+    // member.save((err) => {
+    //     if(err){
+    //         console.error(err);
+    //         common.result.code = 'DRG01';
+    //         common.result.message = common.status.DRG01;
+    //         res.json(common.result);
+    //         return;
+    //     }
+    //     common.result.code = 'DRG00';
+    //     common.result.message = common.status.DRG00;
+    //     res.json(common.result);
+    // });
 });
 
 router.post('/login' , (req , res) => {
