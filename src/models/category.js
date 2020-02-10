6da576464810ed0bc985e15245b3519dc5608f5b
limwoobin <drogba@wink.kr>
2020-02-10 19:17:27 +0900

    menu category add

---
+++ src/models/category.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/mongodb_tutorial');
+
+autoIncrement.initialize(connection);
+
+const categorySchema = new mongoose.Schema({
+    id: {type: Number , required:true , unique:true},
+    name: {type:String}
+});
+
+categorySchema.plugin(autoIncrement.plugin , {
+    model: 'category',
+    field: 'id',
+    startAt: 0,
+    increment: 1
+});
+
+module.exports = mongoose.model('category' , categorySchema);
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const memberRouter = require('./member/MemberController');
-const customerRouter = require('./customer/CustomerController');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
 
+const memberRouter = require('./member/MemberController');
+const customerRouter = require('./customer/CustomerController');
+const categoryRouter = require('./category/CategoryController');
 
 console.log('Router...')
 router.use(upload.array());
@@ -13,5 +14,6 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 router.use('/member' , memberRouter);
 router.use('/customer' , customerRouter);
+router.use('/category' , categoryRouter);
 
 module.exports = router;
