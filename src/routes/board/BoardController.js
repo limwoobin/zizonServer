eb953b909cd948685800425f2d0a6d11f43fee8f
limwoobin <drogba02@naver.com>
2020-03-03 18:01:25 +0900

    자유게시판 불러오기 컨트롤러 추가 및 테스트

--- src/routes/board/BoardController.js
+++ src/routes/board/BoardController.js
@@ -4,5 +4,22 @@ const Board = require('../../models/board');
 const common = require('../../common/common');
 
 
+router.get('/list' , (req , res) => {
+    console.log('board...')
+    const result = common.result;
+    result.code = 'DR00';
+    result.message = common.status.DR00;
+    
+    Board.find((err , boards) => {
+        if(err){
+            result.code = 'DR01';
+            result.message = common.status.DR01;
+            result.data = err;
+            return result;
+        } 
+        result.data = boards;
+    })
+    return res.json(result);
+})
 
 module.exports = router;
--- src/test.js
+++ src/test.js
@@ -1,1 +1,9 @@
-console.log('asd');
+var EventEmitter = require('events').EventEmitter;
+
+function StreamLibrary(resourceName) { 
+  this.emit('start');
+
+  // 파일을 읽고 읽은 청크마다 다음을 수행한다.       
+  this.emit('data', chunkRead);       
+}
+StreamLibrary.prototype.__proto__ = EventEmitter.prototype;   // EventEmitter를 상속받는다
