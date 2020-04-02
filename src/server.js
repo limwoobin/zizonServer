be55f4ca1af653da7bea351fb911603cf43f2d0d
limwoobin <drogba02@naver.com>
2020-04-02 20:30:49 +0900

    미들웨어 소스위치 수정

--- src/server.js
+++ src/server.js
@@ -5,10 +5,9 @@ const db = require('./routes/dbConnection');
 const router = require('./routes/router');
 const setting = require('./routes/setting');
 const expressErrorHandler = require('express-error-handler');
-const logger = require('morgan');
+const morgan = require('morgan');
 const expressSession = require('express-session');
 const visitor = require('./visitor/VisitorFunc');
-
 const errorHandler = expressErrorHandler({
     static: {
         '404':'./public/404.html'
@@ -27,24 +26,21 @@ app.use(expressSession({
     }
 }));
 
-app.use(function(req , res , next){
-    console.log('request URL:' + req.url);
-    next();
-})
 app.get('/' , visitor.visitorCount);
-
 app.use('/' , express.static(__dirname + "/../../client/build"));
 // 기존 클래스버전
 
 // app.use('/' , express.static(__dirname + "/../../../appHooks/build"));
 // 훅스버전
 
-app.use('/dr' , router);
+app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/dr' , router);
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
-app.use(logger('local'));
+
+
 
 const port = process.env.PORT || 4000;
 app.listen(port , () => {
